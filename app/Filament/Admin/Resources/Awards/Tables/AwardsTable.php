5cc085b531cce68ca96d1875f86d181c2996e383
Dmytro Sakharuk <saharin.y@gmail.com>
2026-09-12 22:06:42 +0300

    Add award merging functionality: implement bulk action to merge awards, create AwardMerger service and interface, and add corresponding tests

--- app/Filament/Admin/Resources/Awards/Tables/AwardsTable.php
+++ app/Filament/Admin/Resources/Awards/Tables/AwardsTable.php
@@ -2,14 +2,20 @@
 
 namespace App\Filament\Admin\Resources\Awards\Tables;
 
+use App\Contracts\Contracts\AwardMerger;
 use App\Filament\Admin\Resources\Awardees\AwardeeResource;
 use App\Models\Award;
+use Filament\Actions\BulkAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Notifications\Notification;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
 
 class AwardsTable
 {
@@ -45,6 +51,36 @@ class AwardsTable
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
+                    BulkAction::make('mergeAwards')
+                        ->label(__('Merge awards'))
+                        ->icon(Heroicon::OutlinedArrowsPointingIn)
+                        ->requiresConfirmation()
+                        ->modalHeading(__('Merge awards'))
+                        ->modalDescription(__('The awardees of the selected awards are linked to the award with the most awardees. The other selected awards are deleted.'))
+                        ->modalSubmitActionLabel(__('Merge'))
+                        ->deselectRecordsAfterCompletion()
+                        ->action(function (Collection $records, AwardMerger $awardMerger): void {
+                            try {
+                                $primaryAward = $awardMerger->merge($records);
+                            } catch (InvalidArgumentException $exception) {
+                                Notification::make()
+                                    ->danger()
+                                    ->title(__('Unable to merge awards'))
+                                    ->body($exception->getMessage())
+                                    ->send();
+
+                                return;
+                            }
+
+                            Notification::make()
+                                ->success()
+                                ->title(__('Awards merged'))
+                                ->body(__('The awardees are linked to ":award" now. Deleted awards: :count', [
+                                    'award' => $primaryAward->name,
+                                    'count' => $records->count() - 1,
+                                ]))
+                                ->send();
+                        }),
                     DeleteBulkAction::make(),
                 ]),
             ]);
