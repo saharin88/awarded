6cac11dd4e0c089b4d5131c65a7c1c2951403655
Dmytro Sakharuk <saharin.y@gmail.com>
2026-09-13 00:23:08 +0300

    Refactor Awardee and Decree resources: update navigation icons and improve URL filtering logic for decree number

--- app/Filament/Admin/Resources/Awardees/AwardeeResource.php
+++ app/Filament/Admin/Resources/Awardees/AwardeeResource.php
@@ -20,7 +20,7 @@ class AwardeeResource extends Resource
 
     protected static ?string $model = Awardee::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUserGroup;
 
     protected static ?string $navigationLabel = 'Awardees';
 
